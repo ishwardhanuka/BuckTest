71c045615a7db4d1fe32a58916f52778aad9a25e
Ishwar Dhanuka <ishwardhanuka@gmail.com>
2019-03-29 16:09:35 +0800

    Initial Commit

---
+++ BuckTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuckTest
+//
+//  Created by Ishwar Dhanuka on 29/3/19.
+//  Copyright © 2019 Ishwar Dhanuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
