885d2706bb8a898c28d9b4eb15a89f2a14252782
Steven Lee <Steven.armand.lee@gmail.com>
2019-04-29 11:10:06 +0800

    added iglistkit example

--- BuckTest/ViewController.swift
+++ BuckTest/ViewController.swift
@@ -13,10 +13,10 @@ import Alamofire
 //import FirebaseDatabase
 //import FirebaseMessaging
 //import FirebaseInstanceID
-//import IGListKit
-//import SwinjectStoryboard
-//import RxSwift
-//import RxCocoa
+import IGListKit
+import SwinjectStoryboard
+import RxSwift
+import RxCocoa
 //import ZDCChat
 //import Fabric
 //import Crashlytics
@@ -37,12 +37,15 @@ import Alamofire
 //import ZendeskCoreSDK
 //import ZendeskProviderSDK
 class ViewController: UIViewController {
-    @IBOutlet weak var textView: UITextView!
-    //    let countryPicker = CountryPicker()
+    @IBOutlet var collectionView: UICollectionView!
+    lazy var adapter: ListAdapter = {
+        return ListAdapter(updater: ListAdapterUpdater(), viewController: self)
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        textView.text = "aa"
+        adapter.collectionView = collectionView
+        adapter.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
         
         Alamofire.request("https://www.google.com")
@@ -50,99 +53,6 @@ class ViewController: UIViewController {
 
 
 }
-
-//apple_third_party_lib(
-//    name = "Firebase",
-//    exported_headers = glob([
-//        "Firebase/**/*.h",
-//        "FirebaseAnalytics/**/*.h",
-//        "FirebaseAnalyticsInterop/**/*.h",
-//        "FirebaseAuth/**/*.h",
-//        "FirebaseAuthInterop/**/*.h",
-//        "GoogleUtilities/GoogleUtilities/UserDefaults/Private/*.h",
-//        "FirebaseCore/Firebase/Core/Public/*.h",
-//        "FirebaseAuthInterop/Interop/Auth/Public/FIRAuthInterop.h",
-//        "FirebaseCore/Firebase/Core/Private/FIRAppInternal.h",
-//        "FirebaseCore/Firebase/Core/Private/FIRLogger.h",
-//        "FirebaseAnalyticsInterop/Interop/Analytics/Public/FIRAnalyticsInterop.h",
-//        "GTMSessionFetcher/Source/GTMSessionFetcher.h",
-//        "Protobuf/objectivec/google/protobuf/Any.pbobjc.h",
-//        "FirebaseCore/**/*.h",
-//        "FirebaseDatabase/**/*.h",
-//        "FirebaseInstanceID/**/*.h",
-//        "FirebaseMessaging/**/*.h",
-//        "GoogleAppMeasurement/**/*.h",
-//        "GoogleUtilities/**/*.h",
-//        "GTMSessionFetcher/**/*.h",
-//        "leveldb-library/**/*.h",
-//        "nanopb/**/*.h",
-//        "Protobuf/**/*.h",
-//        ], exclude = [
-//            "FirebaseMessaging/Firebase/Messaging/FirebaseMessaging.h",
-//        ]),
-//    srcs = glob([
-//        "Firebase/**/*.mm",
-//        "FirebaseAnalytics/**/*.mm",
-//        "FirebaseAnalyticsInterop/**/*.mm",
-//        "FirebaseAuth/**/*.mm",
-//        "FirebaseAuthInterop/**/*.mm",
-//        "FirebaseCore/**/*.mm",
-//        "FirebaseDatabase/**/*.mm",
-//        "FirebaseInstanceID/**/*.mm",
-//        "FirebaseMessaging/**/*.mm",
-//        "GoogleAppMeasurement/**/*.mm",
-//        "GoogleUtilities/**/*.mm",
-//        "GTMSessionFetcher/**/*.mm",
-//        "leveldb-library/**/*.mm",
-//        "nanopb/**/*.mm",
-//        "Protobuf/**/*.mm",
-//        "Firebase/**/*.m",
-//        "FirebaseAnalytics/**/*.m",
-//        "FirebaseAnalyticsInterop/**/*.m",
-//        "FirebaseAuth/**/*.m",
-//        "FirebaseAuthInterop/**/*.m",
-//        "FirebaseCore/**/*.m",
-//        "FirebaseDatabase/**/*.m",
-//        "FirebaseInstanceID/**/*.m",
-//        "FirebaseMessaging/**/*.m",
-//        "GoogleAppMeasurement/**/*.m",
-//        "GoogleUtilities/**/*.m",
-//        "GTMSessionFetcher/**/*.m",
-//        "leveldb-library/**/*.m",
-//        "nanopb/**/*.m",
-//        "Protobuf/**/*.m",
-//        "Firebase/**/*.swift",
-//        "FirebaseAnalytics/**/*.swift",
-//        "FirebaseAnalyticsInterop/**/*.swift",
-//        "FirebaseAuth/**/*.swift",
-//        "FirebaseAuthInterop/**/*.swift",
-//        "FirebaseCore/**/*.swift",
-//        "FirebaseDatabase/**/*.swift",
-//        "FirebaseInstanceID/**/*.swift",
-//        "FirebaseMessaging/**/*.swift",
-//        "GoogleAppMeasurement/**/*.swift",
-//        "GoogleUtilities/**/*.swift",
-//        "GTMSessionFetcher/**/*.swift",
-//        "leveldb-library/**/*.swift",
-//        "nanopb/**/*.swift",
-//        "Protobuf/**/*.swift",
-//        "Firebase/**/*.framework",
-//        "FirebaseAnalytics/**/*.framework",
-//        "FirebaseAnalyticsInterop/**/*.framework",
-//        "FirebaseAuth/**/*.framework",
-//        "FirebaseAuthInterop/**/*.framework",
-//        "FirebaseCore/**/*.framework",
-//        "FirebaseDatabase/**/*.framework",
-//        "FirebaseInstanceID/**/*.framework",
-//        "FirebaseMessaging/**/*.framework",
-//        "GoogleAppMeasurement/**/*.framework",
-//        "GoogleUtilities/**/*.framework",
-//        "GTMSessionFetcher/**/*.framework",
-//        "leveldb-library/**/*.framework",
-//        "nanopb/**/*.framework",
-//        "Protobuf/**/*.framework",
-//        ]),
-//)
 extension String {
     var htmlToAttributedString: NSAttributedString? {
         guard let data = data(using: .utf8) else { return NSAttributedString() }
@@ -155,4 +65,20 @@ extension String {
     var htmlToString: String {
         return htmlToAttributedString?.string ?? ""
     }
+}
+
+extension ViewController: ListAdapterDataSource {
+    func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
+        return ["a", "b"] as [ListDiffable]
+    }
+    
+    func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
+        return LabelSectionController()
+    }
+    
+    func emptyView(for listAdapter: ListAdapter) -> UIView? {
+        return nil
+    }
+    
+    
 }
